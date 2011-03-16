4e6741f7fb7a01c39ee38b0b7f9e5884426ab852
wei2000zhou <wei2000zhou@126.com>
2011-03-17 00:36:54 +0800

    2011-3-17-24:36-模板；忽略文件

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Um::Application.config.secret_token = '3a43e69a57329242efab39d4d934e73cedd448758a7a2beb37671f4a4b768a4da7e7f1e2215567aabfecfa2b1473df5a5709857be91fbcaca1d583e872928d33'
